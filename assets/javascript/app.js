119ad5154e6cf6d29f22bc84ba9bd407af60c40b
Kenneth Chung <kennethchung5@gmail.com>
2018-10-26 19:46:36 -0400

    reconfigured app to work off a copy of questionSet array, rather than choices array per question; updated processQuestion() accordingly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,31 +16,72 @@ var questionTest = {
     imgUrl: "assets/images/abstract-q-c-200-200-9.jpg"
 }
 
+//questionSet will be the array of question-objects
+var questionSet = [
+    
+];
+
+var remainingQuestions = [];
+
 var correctChoice = "";
 var correctChoiceImg = "";
 
-function processQuestion(questionObj) {
-    //store the correct answer
-    correctChoice = questionObj.choices[0];
-    correctChoiceImg = questionObj.imgUrl;
+// called when start or restart game button is clicked
+// copies questionSet 
+function startGame() {
+    remainingQuestions = questionSet;
 
-    // console.log("The url is " + correctChoiceImg);
-    //display the questionText in the questionDiv
-    $("#questionDiv").text(questionObj.questionText);    
+
+};
+
+
+function processQuestion() {
+// choosing the word, and checking for end-of-game
+
+    if (remainingQuestions.length === 0) {
+        // end of game scenario
+    }
+    else {
+        var randQIndex = Math.floor(Math.random() * remainingQuestions.length);
+
+        var currentQuestion = remainingQuestions[randQIndex];
+
+        remainingQuestions.splice(randQIndex, 1);
+
+        // what to do with the selected word
+            //store the correct answer
+        correctChoice = currentQuestion.choices[0];
+        correctChoiceImg = currentQuestion.imgUrl;
+
+        //display the questionText in the questionDiv
+        $("#questionDiv").text(currentQuestion.questionText);    
     
-    //"shuffle" choices array;; here, work with a copy of the choices array, instead of calling methods on the original; preserving the original array allows the game to be replayed without reloading the page    
-    var choicesCopy = questionObj.choices;
+
+        //"shuffle" choices array;; here, work with a copy of the choices array, instead of calling methods on the original; preserving the original array allows the game to be replayed without reloading the page    
+        //UPDATE: no need to make a copy of the choices array; we instead made a copy of the questionSet array
+        
+        // this for loop selects each element of currentQuestion.choices, in a random order, and creates a div using the elements' text
+        for (var i = currentQuestion.choices.length; i > 0; i--) {
+            var randCIndex = Math.floor(Math.random() * i);
+
+            var choiceDiv = $("<div>").text(currentQuestion.choices[randCIndex]);
+            choiceDiv.addClass("choice");
+            $("#centralDisplay").append(choiceDiv);
+
+            currentQuestion.choices.splice(randCIndex, 1);
+        };
+
+    }
+
+
+
     
-    // this for loop selects each element of choicesCopy, in a random order, and creates a div using the elements' text
-    for (var i = choicesCopy.length; i > 0; i--) {
-        var randIndex = Math.floor(Math.random() * i);
 
-        var choiceDiv = $("<div>").text(choicesCopy[randIndex]);
-        choiceDiv.addClass("choice");
-        $("#centralDisplay").append(choiceDiv);
 
-        choicesCopy.splice(randIndex, 1);
-    };
+    
+    
+    
+
 };
 
 
